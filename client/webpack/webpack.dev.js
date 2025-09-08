3b1c7060f7a05bf21744d16445015b7510e6928e
Charles2404200 <leanhminh2404@gmail.com>
2025-09-09 01:21:48 +0700

    adjust ip

--- client/webpack/webpack.dev.js
+++ client/webpack/webpack.dev.js
@@ -78,7 +78,7 @@ const config = {
   devServer: {
     // Configuration for webpack-dev-server
     port: 8080,
-    host: '34.227.19.111', // Listen on all network interfaces
+    host: '54.163.245.207', // Listen on all network interfaces
     open: true,
     inline: true,
     compress: true,
