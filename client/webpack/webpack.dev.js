8f35f42e48061d8256f5bcad797ecad56d736a36
APotato4325 <s4019570@rmit.edu.vn>
2025-09-15 01:29:37 +0700

    test port

--- client/webpack/webpack.dev.js
+++ client/webpack/webpack.dev.js
@@ -78,7 +78,7 @@ const config = {
   devServer: {
     // Configuration for webpack-dev-server
     port: 8080,
-    host: 'localhost', // Listen on all network interfaces
+    host: '54.242.180.228', // Listen on all network interfaces
     open: true,
     inline: true,
     compress: true,
